619e96abffb66c28e01003329deb35fa2a39c8a5
Paweł Zajączkowski <pawel.zajaczkowski@logintegra.com>
2021-11-23 14:26:05 +0100

    generate view i controller Album i Song

--- grails-app/controllers/com/gtunes/AlbumController.groovy
+++ grails-app/controllers/com/gtunes/AlbumController.groovy
@@ -4,6 +4,8 @@ import org.springframework.dao.DataIntegrityViolationException
 
 class AlbumController {
 
+    static scaffold = true
+
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
     def index() {
